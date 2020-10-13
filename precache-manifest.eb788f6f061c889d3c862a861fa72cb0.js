fef3295d811a7bf3981b230520db247b26d3b96c
Yarik1993 <makarov1603@icloud.com>
2020-10-13 08:55:46 +0300

    Updates

---
+++ precache-manifest.eb788f6f061c889d3c862a861fa72cb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8fd8f8cc556960ed5ec065c9c67bad",
+    "url": "/pie-chart/index.html"
+  },
+  {
+    "revision": "0c332bd04c3310f1da2c",
+    "url": "/pie-chart/static/css/main.fa65609b.chunk.css"
+  },
+  {
+    "revision": "ab56f971524bc340bdb2",
+    "url": "/pie-chart/static/js/2.3e34c191.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pie-chart/static/js/2.3e34c191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c332bd04c3310f1da2c",
+    "url": "/pie-chart/static/js/main.7a0e4fa4.chunk.js"
+  },
+  {
+    "revision": "edc5f7056ed27d9c6b5e",
+    "url": "/pie-chart/static/js/runtime-main.651f6f9b.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/pie-chart/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/pie-chart/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
